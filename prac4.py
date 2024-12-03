3a7fc41740a7170425d2650054430ca6cdfa718e
MAY-GOH <P4024@TARC.EDU.MY>
2024-12-03 15:06:01 +0800

    Update prac4.py

--- prac4.py
+++ prac4.py
@@ -181,3 +181,212 @@ for count in range(repitition):
   total = (base_value + total) * (1 + monthly_interest)
 
 print("{:s} {:.2f}".format("After the sixth month, the account value is RM", total))
+
+# Question 8
+emoji_code_right = 128072
+emoji_code_left = 128073
+separator = ""
+
+number = int(input("Enter a positive integer: "))
+
+print("\n" + chr(emoji_code_left), "Shape #1", chr(emoji_code_right))
+for i in range(1, number + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+print("\n" + chr(emoji_code_left), "Shape #2", chr(emoji_code_right))
+for i in range(1, number + 1):
+    for j in range(1, (number + 2) - i):
+        print(j, end=" ")
+    print()
+
+print("\n" + chr(emoji_code_left), "Shape #3", chr(emoji_code_right))
+for i in range(1, number + 1):
+    for j in range(number, 0, -1):
+        if i < j:
+            print(" ", end=" ")
+        else:            
+            print(j, end=" ")
+    print()
+
+print("\n" + chr(emoji_code_left), "Shape #4", chr(emoji_code_right))
+for i in range(1, number + 1):
+    for j in range(1, number + 1):
+        if i > j:
+            print(" ", end=" ")
+        else:            
+            print(j, end=" ")
+    print()
+    
+# Question 9
+
+number = int(input("Enter a positive integer: "))
+
+for i in range(1, number + 1):
+    total = 0
+    for j in range(1, i + 1):
+        if j == i:
+            print(j, end="")
+        else:
+            print(j, "+ ", end="")
+        total += j
+    print(" =", total)
+    
+# Question 10
+QUANTITY = 4
+counter = 0
+
+print("QUIZ")
+
+for i in range(QUANTITY):
+    print("-" * QUANTITY * 10)
+    print("Q" + str(i + 1) + ":", end=" ")
+
+    match i:
+        case 0:
+            print("12 + 8 = ?")
+            print("A. 14\tB. 16\tC. 18\tD. 20")
+            answer = input("\nEnter your answer (A or B or C or D): ")
+            
+            if answer == "D" or answer == "d":
+                print("Correct!")
+                counter += 1
+            else:
+                print("Wrong!")
+
+        case 1: 
+            print("15 - 7 = ?")
+            print("A. 6\tB. 7\tC. 8\tD. 9")
+            answer = input("\nEnter your answer (A or B or C or D): ")
+            
+            if answer == "C" or answer == "c":
+                print("Correct!")
+                counter += 1
+            else:
+                print("Wrong!")
+            
+        case 2:
+            print("9 x 6 = ?")
+            print("A. 45\tB. 48\tC. 54\tD. 60")
+            answer = input("\nEnter your answer (A or B or C or D): ")
+            
+            if answer == "C" or answer == "c":
+                print("Correct!")
+                counter += 1
+            else:
+                print("Wrong!")
+            
+        case 3:
+            print("40 / 5 = ?")
+            print("A. 7\tB. 8\tC. 9\tD. 10")
+            answer = input("\nEnter your answer (A or B or C or D): ")
+            
+            if answer == "B" or answer == "b":
+                print("Correct!")
+                counter += 1
+            else:
+                print("Wrong!")
+            
+    if i == QUANTITY - 1:
+        print("-" * 40)
+    
+if counter == 4:
+    print("Conclusion: Congratulations! You answered all the questions correctly!")
+else:
+    print("Conclusion: Keep practicing, and you'll improve!")
+    
+# Question 11
+import random
+
+counter = 0
+user_win = 0
+user_lose = 0
+draw = 0
+
+while True:
+    # 0, 1, or 2 represents "scissors", "rock", and "paper" respectively
+    answer = random.randint(0, 2) 
+    
+    print("0: scissors, 1: rock, and 2: paper")
+    while True:
+        choice = int(input("Enter your choice: "))
+        if choice == 0 or choice == 1 or choice == 2:
+            break
+        else:
+            print("Wrong input...")
+    
+    match answer:
+        case 0:
+            print("Computer's choice: Scissors")
+        case 1: 
+            print("Computer's choice: Rock")
+        case 2:
+            print("Computer's choice: Paper")
+    
+    if choice == answer:
+        print("Draw!")
+        draw += 1
+    elif (choice == 0 and answer == 2) or (choice == 1 and answer == 0) or (choice == 2 and answer == 1):
+        print("You win!")
+        user_win += 1 
+    else:
+        print("You lose!")
+        user_lose += 1
+    
+    counter += 1
+    
+    print()
+    if (counter > 2) and (user_win != user_lose):
+        print("You won", user_win, "times" if user_win > 1 else "time", end=" ")
+        print("and computer won", user_lose, "times." if user_lose > 1 else  "time.")
+        if user_win > user_lose:
+            print("You are the final winner!")
+        else:
+            print("Computer is the final winner!")
+        break
+        
+# Question 12
+
+import random
+
+digit_1 = random.randint(0, 9) 
+digit_2 = random.randint(0, 9) 
+digit_3 = random.randint(0, 9) 
+
+guess = int(input("Enter a 3-digit integer: "))
+
+guess_3 = guess % 10
+answer = guess // 10
+guess_2 = answer % 10
+guess_1 = answer // 10
+
+if digit_1 == guess_1 and digit_2 == guess_2 and digit_3 == guess_3:
+    print("Congratulations! You won RM10,000!")
+elif (digit_1 == guess_1 and digit_2 == guess_3 and digit_3 == guess_2) or \
+     (digit_1 == guess_2 and digit_2 == guess_1 and digit_3 == guess_3) or \
+     (digit_1 == guess_2 and digit_2 == guess_3 and digit_3 == guess_1) or \
+     (digit_1 == guess_3 and digit_2 == guess_2 and digit_3 == guess_1) or \
+     (digit_1 == guess_3 and digit_2 == guess_1 and digit_3 == guess_2):
+    print("Congratulations! You win RM3,000!") 
+elif (digit_1 == guess_1 and digit_2 != guess_2 and digit_3 != guess_3) or \
+     (digit_1 == guess_1 and digit_2 != guess_3 and digit_3 != guess_2) or \
+     (digit_1 == guess_2 and digit_2 != guess_1 and digit_3 != guess_3) or \
+     (digit_1 == guess_2 and digit_2 != guess_3 and digit_3 != guess_1) or \
+     (digit_1 == guess_3 and digit_2 != guess_2 and digit_3 != guess_1) or \
+     (digit_1 == guess_3 and digit_2 != guess_1 and digit_3 != guess_2) or \
+     (digit_2 == guess_1 and digit_1 != guess_2 and digit_3 != guess_3) or \
+     (digit_2 == guess_1 and digit_1 != guess_3 and digit_3 != guess_2) or \
+     (digit_2 == guess_2 and digit_1 != guess_1 and digit_3 != guess_3) or \
+     (digit_2 == guess_2 and digit_1 != guess_3 and digit_3 != guess_1) or \
+     (digit_2 == guess_3 and digit_1 != guess_2 and digit_3 != guess_1) or \
+     (digit_2 == guess_3 and digit_1 != guess_1 and digit_3 != guess_2) or \
+     (digit_3 == guess_1 and digit_2 != guess_2 and digit_1 != guess_3) or \
+     (digit_3 == guess_1 and digit_2 != guess_3 and digit_1 != guess_2) or \
+     (digit_3 == guess_2 and digit_2 != guess_1 and digit_1 != guess_3) or \
+     (digit_3 == guess_2 and digit_2 != guess_3 and digit_1 != guess_1) or \
+     (digit_3 == guess_3 and digit_2 != guess_2 and digit_1 != guess_1) or \
+     (digit_3 == guess_3 and digit_2 != guess_1 and digit_1 != guess_2):
+    print("Congratulations! You win RM1,000!") 
+else:
+    print("I regret to inform you that your ticket did not win a prize this time.") 
