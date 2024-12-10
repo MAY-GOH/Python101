fcc0946be2adf03a7713991e2dd215684d1cf516
MAY-GOH <P4024@TARC.EDU.MY>
2024-12-10 13:34:03 +0800

    Update prac6.py

--- prac6.py
+++ prac6.py
@@ -40,3 +40,45 @@ while True:
                     print("You have entered a wrong input!")
 
 print("Thank you!")
+
+# Question 2
+while True:
+    # Prompt the user to enter a score
+    score = input("Enter a score or 'Q'/'q' to exit: ")
+
+    if score == 'Q' or score == 'q':
+        break
+        
+    elif not score.replace(".","").isnumeric():
+      print("Only positive number is allowed")
+        
+    else:
+        score = float(score)
+
+        if not (score >=0 and score <=100):
+            print("Only 0 ≤ number ≤ 100 inclusive are allowed!\n")
+        else:
+            # Determine the grade based on the score
+            if score >= 90 and score <= 100:
+                grade = 'A'
+            elif score >= 80 and score < 90:
+                grade = 'B'
+            elif score >= 70 and score < 80:
+                grade = 'C'
+            elif score >= 60 and score < 70:
+                grade = 'D'
+            elif score >= 0 and score < 60:
+                grade = 'F'
+            else:
+                grade = ''
+
+            # Print an output for the grade determined
+            match grade:
+                case 'A' | 'F':
+                    print("You got an", grade, "for the test!")
+                case 'B' | 'C' | 'D':
+                    print("You got a", grade, "for the test!")
+                case _: 
+                    print("You have entered a wrong input!")
+
+print("Thank you!")
