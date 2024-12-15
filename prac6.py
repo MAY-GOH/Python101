abafc31435ccc48aa0d547bbf4319fa12cd17a8c
MAY-GOH <P4024@TARC.EDU.MY>
2024-12-15 08:19:50 +0800

    Update prac6.py

--- prac6.py
+++ prac6.py
@@ -257,3 +257,132 @@ print("Numbers (ascending order): ", end= "")
 for number in numbers:
   print(number, end = " ")
 
+# Question 8
+import random
+
+state_capital = {
+    "Johor": "Johor Bahru", "Kedah": "Alor Setar", "Kelantan": "Kota Bharu",
+    "Malacca": "Malacca City", "Negeri Sembilan": "Seremban",
+    "Pahang": "Kuantan", "Penang": "George Town", "Perak": "Ipoh",
+    "Perlis": "Kangar", "Sabah": "Kota Kinabalu", "Sarawak": "Kuching",
+    "Selangor": "Shah Alam", "Terengganu": "Kuala Terengganu",
+    "Kuala Lumpur": "Kuala Lumpur", "Labuan": "Victoria", "Putrajaya": "Putrajaya"
+}
+
+count = 0
+keys = list(state_capital.keys())
+random.shuffle(keys)
+
+print("Identify the capital of following states:")
+for i in range(len(keys)):
+    string = "\nEnter the capital of {:16}: ".format(keys[i])    
+    if input(string).lower() == state_capital[keys[i]].lower():
+        print("Correct")
+        count += 1
+    else:
+        print("Wrong")
+
+print("\nYou answered {:d} {:s} correctly!".format(count, "questions" if count > 1 else "question"))
+
+# Question 9
+import random
+
+digit_1 = random.randint(0, 9) 
+digit_2 = random.randint(0, 9) 
+digit_3 = random.randint(0, 9) 
+
+print("Lottery number:", str(digit_1) + str(digit_2) + str(digit_3))
+digits = [digit_1, digit_2, digit_3]
+
+while True:
+    try:
+        guess = int(input("Enter a 3-digit integer: "))
+    except:
+        print("Only integers are acceptable!\n")
+    else:
+        if guess > 999:
+            print("Only 3-digit integers are acceptable!\n")
+        else:
+            break
+
+guess_3 = guess % 10
+answer = guess // 10
+guess_2 = answer % 10
+guess_1 = answer // 10        
+guesses = [guess_1, guess_2, guess_3]
+
+if digits == guesses:
+    print("Congratulations! You won RM10,000!")
+elif digits != guesses and sorted(digits) == sorted(guesses):
+    print("Congratulations! You win RM3,000!") 
+elif len(set(digits) & set(guesses)) > 0: 
+    print("Congratulations! You win RM1,000!") 
+else:
+    print("I regret to inform you that your ticket did not win a prize this time.") 
+
+# Quesiton 10
+# ♠ = Spades, ♥ = Hearts, ♣ = Clubs, ♦ = Diamonds
+deck = {
+    1: ('♠', 'A'), 2: ('♠', '2'), 3: ('♠', '3'), 4: ('♠', '4'), 
+    5: ('♠', '5'), 6: ('♠', '6'), 7: ('♠', '7'), 8: ('♠', '8'), 
+    9: ('♠', '9'), 10: ('♠', '10'), 11: ('♠', 'J'), 12: ('♠', 'Q'), 
+    13: ('♠', 'K'), 14: ('♥', 'A'), 15: ('♥', '2'), 16: ('♥', '3'), 
+    17: ('♥', '4'), 18: ('♥', '5'), 19: ('♥', '6'), 20: ('♥', '7'), 
+    21: ('♥', '8'), 22: ('♥', '9'), 23: ('♥', '10'), 24: ('♥', 'J'), 
+    25: ('♥', 'Q'), 26: ('♥', 'K'), 27: ('♣', 'A'), 28: ('♣', '2'), 
+    29: ('♣', '3'), 30: ('♣', '4'), 31: ('♣', '5'), 32: ('♣', '6'), 
+    33: ('♣', '7'), 34: ('♣', '8'), 35: ('♣', '9'), 36: ('♣', '10'), 
+    37: ('♣', 'J'), 38: ('♣', 'Q'), 39: ('♣', 'K'), 40: ('♦', 'A'), 
+    41: ('♦', '2'), 42: ('♦', '3'), 43: ('♦', '4'), 44: ('♦', '5'), 
+    45: ('♦', '6'), 46: ('♦', '7'), 47: ('♦', '8'), 48: ('♦', '9'), 
+    49: ('♦', '10'), 50: ('♦', 'J'), 51: ('♦', 'Q'), 52: ('♦', 'K')
+}
+
+numbers = random.sample(list(deck.keys()), 4)
+total = 0
+
+print("Cards: ", end="")
+for i in range(len(numbers)):
+    print(deck[numbers[i]][0] + deck[numbers[i]][1], end="\n" if i == len(numbers) - 1 else " and ")
+    match deck[numbers[i]][1]:
+        case 'A':
+            total += 1
+        case 'J':
+            total += 11
+        case 'Q':
+            total += 12
+        case 'K':
+            total += 13
+        case _:
+            total += int(deck[numbers[i]][1])
+print("Sum  :", total)
+
+# Question 11
+question_1 = "12 + 8 = ?\nA. 14\tB. 16\tC. 18\tD. 20"
+question_2 = "15 - 7 = ?\nA. 6\tB. 7\tC. 8\tD. 9"
+question_3 = "9 x 6 = ?\nA. 45\tB. 48\tC. 54\tD. 60"
+question_4 = "12 + 8 = ?\nA. 14\tB. 16\tC. 18\tD. 20"
+
+quizzes = {1: (question_1, 'D'), 2: (question_2, 'C'), 3: (question_3, 'C'), 4: (question_4, 'B')}
+counter = 0
+
+print("QUIZ")
+
+for i in range(len(quizzes.keys())):
+    print("-" * len(quizzes) * 10)
+    print("Q" + str(i + 1) + ":", quizzes[i+1][0])
+    
+    while True:
+        answer = input("Enter your answer (A or B or C or D): ")
+        answer = answer.upper()
+        if answer == 'A' or answer == 'B' or answer == 'C' or answer == 'D':
+            break
+
+    if answer == quizzes[i+1][1]:
+        print("Correct!")
+        counter += 1
+    else:
+        print("The correct answer is", quizzes[i+1][1])
+    
+print("-" * len(quizzes) * 10)
+print("You answered {:d} question{:s} correctly!".format(counter, "" if counter < 2 else "s"))
